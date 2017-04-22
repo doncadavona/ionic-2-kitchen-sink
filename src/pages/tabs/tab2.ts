5a481c35f0e1cb2ae7b6ec8d2c8807423e25ed96
Don Cadavona <dcadavona@gmail.com>
2017-04-22 18:26:37 +0800

    Finished tabs page.

--- src/pages/tabs/tab2.ts
+++ src/pages/tabs/tab2.ts
@@ -9,7 +9,7 @@ import { NavController } from 'ionic-angular';
     </ion-navbar>
   </ion-header>
   <ion-content padding>
-    <h1>Tab 2</h1>
+    <h1>Stars</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
